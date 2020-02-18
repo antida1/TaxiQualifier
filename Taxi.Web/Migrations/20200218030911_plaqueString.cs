1139702ea8c15e3956486c55cbcb0f8e41126bbf
LINA PATRICIA DAVID GONZALEZ <linadavid212820@correo.itm.edu.co>
2020-02-17 23:56:32 -0500

    Improve taxis controller

---
+++ Taxi.Web/Migrations/20200218030911_plaqueString.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Taxi.Web.Migrations
+{
+    public partial class plaqueString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Plaque",
+                table: "Taxis",
+                maxLength: 6,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldMaxLength: 6);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Plaque",
+                table: "Taxis",
+                maxLength: 6,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 6);
+        }
+    }
+}
